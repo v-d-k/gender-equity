2b36e1a809badcb44ae41c646b2d2f9ee079426a
Kachavi <vishaldineshbhaikacha@gmail.com>
2023-07-14 23:17:16 +0530

    final commit

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// Change the background color of the section on scroll
+
 window.addEventListener('scroll', function() {
     var section = document.querySelector('.color-change-section');
     var sectionHeight = section.offsetHeight;
     var scrollPosition = window.pageYOffset;
   
-    var startColor = [250, 253, 93]; // RGB value of the starting color (blue)
-    var endColor = [141, 196, 235]; // RGB value of the ending color (red)
+    var startColor = [237, 137, 111];
+    var endColor = [141, 196, 235]; 
   
-    // Calculate the scroll progress
+    
     var scrollProgress = scrollPosition / sectionHeight;
   
-    // Interpolate the RGB values based on the scroll progress
+
     var interpolatedColor = interpolateColors(startColor, endColor, scrollProgress);
   
-    // Set the background color of the section
+
     section.style.backgroundColor = 'rgb(' + interpolatedColor.join(',') + ')';
   });
   
-  // Function to interpolate between two colors
+
   function interpolateColors(startColor, endColor, progress) {
     var r = Math.round(startColor[0] + (endColor[0] - startColor[0]) * progress);
     var g = Math.round(startColor[1] + (endColor[1] - startColor[1]) * progress);
@@ -36,17 +36,3 @@ window.addEventListener('scroll', function() {
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
-
-function handleScroll() {
-  var elements = document.querySelectorAll('.fade-in-up');
-  elements.forEach(function(element) {
-    if (isElementInViewport(element)) {
-      element.classList.add('fade-in-up-visible');
-    }
-  });
-}
-
-// Initial check on page load
-window.addEventListener('load', handleScroll);
-// Check on scroll
-window.addEventListener('scroll', handleScroll);
